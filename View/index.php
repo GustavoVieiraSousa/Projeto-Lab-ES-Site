a23aa782b83ad260dbdc021257cf69cef1b1aa50
GustavoVieiraSousa <gustavovieirasousa1v1@gmail.com>
2025-06-18 07:38:34 -0300

    adição pequena

--- View/index.php
+++ View/index.php
@@ -1,9 +1,29 @@
 <!DOCTYPE html>
-<html lan="en">
+<html lang="en">
     <head>
-        <meta charset=utf-8>
+        <meta charset="utf-8">
         <title>Home PokeParty</title>
         <link rel="stylesheet" href="./Style/styleHome.css">
+        <style>
+            .corpo {
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                margin-top: 40px;
+            }
+            .corpo h2 {
+                color: #2a75bb;
+                text-align: center;
+                margin-bottom: 18px;
+            }
+            .corpo p {
+                color: #2a75bb;
+                font-size: 1.15rem;
+                max-width: 600px;
+                text-align: center;
+                margin-top: 18px;
+            }
+        </style>
     </head>
 
     <body>
@@ -23,8 +43,15 @@
           </div>
         </div>
       </nav>
+
         <main class="corpo">
-            <h6></h6>
+          <br>
+          <br>
+            <h2>Bem-vindo ao PokéParty!</h2>
+            <p>
+                PokéParty é uma plataforma online onde você pode montar seus próprios times de Pokémon, desafiar amigos em batalhas por turnos, explorar a Pokédex e participar de salas de batalha.<br>
+                Crie estratégias, escolha seus Pokémon favoritos e mostre que você é um verdadeiro mestre Pokémon!
+            </p>
         </main>
     </body>
 </html>
